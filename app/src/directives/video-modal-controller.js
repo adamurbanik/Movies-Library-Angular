57f4d7c8b494a5a72554745f54649ecfae748213
Adam Urbanik <adamurbanik@gmail.com>
2016-03-17 13:50:58 +0100

    modyfikacja dyrektywy video, dodanie kontrolera do dyrektywy

--- app/src/directives/video-modal-controller.js
+++ app/src/directives/video-modal-controller.js
@@ -8,7 +8,6 @@
 
     VideoModalController.prototype.changeSize = function changeSize(route) {
        this.isSmall =  !this.isSmall;
-       console.log(this, this.visible);
     };
 
     return VideoModalController;
